6be087b5917d8b269ee9ad1ba80919645d56d7de
berdullayev@gmail.com <macbookpro@MACBOOKnoMacBook-Pro.local>
2026-04-25 08:05:14 +0500

    q

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Navbar(props){
+    return<>
+    <h2 style={{textAlign:"center"}}>{props.age}: your age</h2>
+     <h1 style={{textAlign:"center"}}>{props.name}</h1>
+     </>
+
+}
